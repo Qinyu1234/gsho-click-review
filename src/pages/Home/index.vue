e0b74d78c3357e6a2cc86219fefb533b95500339
qinyu <947794823@qq.com>
2021-11-22 17:37:25 +0800

    '2021/11/22上'

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <TypeNav/>
+        <ListContainer/>
+        <TodayRecommend/>
+        <Rank/>
+        <Like/>
+        <Floor/>
+        <Brand/>
+    </div>
+</template>
+<script>
+import Brand from './Brand'
+import Floor from './Floors'
+import Like from './Like'
+import ListContainer from './ListContainer'
+import Rank from './Rank'
+import TodayRecommend from './TodayRecommend'
+    export default {
+        name:'Home',
+        components:{
+            Brand,
+            Floor,
+            Like,
+            ListContainer,
+            Rank,
+            TodayRecommend
+        }
+    }
+</script>
+<style lang="less" scoped>
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,17 @@
+import Home from "@/pages/Home";
+import Register from "@/pages/Register";
+import Search from "@/pages/Search";
+export default [
+    {
+        path:'/',
+        component:Home
+    },
+    {
+        path:'/register',
+        component:Register
+    },
+    {
+        path:'/search',
+        component:Search
+    }
+]
